e02eba783ba19a3a724803dacce7d2f174175bec
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-02 12:48:10 -0500

    feat: maquetación del Layout

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,19 +9,22 @@ import {
   Success,
   NotFound,
 } from '../containers';
+import Layout from '../components/Layout';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
 
-        <Route component={NotFound} />
-      </Switch>
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
