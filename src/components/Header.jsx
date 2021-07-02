0fa9bff81bc1b7aa851b98a5fd22e7c6582a321e
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-02 17:23:38 -0500

    feat: integración de íconos y conexión con react-router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/components/Header.css';
 
 export default function Header() {
   return (
     <div className="Header">
-      <h1 className="Header-title">Conf-Merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-title">
+        <Link to="/">Conf-Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" />
+        </Link>
+      </div>
     </div>
   );
 }
