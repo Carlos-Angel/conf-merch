1ea2f3164ac64e17495c4bfce7a5dd92dcf2a7d7
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-04 13:02:50 -0500

    feat: custom hook para google maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import config from '../config';
 
-export default function Map() {
+export default function Map({ location }) {
   const mapStyles = {
     height: '50vh',
     width: '100%',
   };
-  const defaultCenter = {
-    lat: 17.552186,
-    lng: -99.50135,
-  };
 
   return (
     <LoadScript googleMapsApiKey={config.googleMapsApiKey}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
-        <Marker position={defaultCenter} />
+      <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={location}>
+        <Marker position={location} />
       </GoogleMap>
     </LoadScript>
   );
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -2,12 +2,15 @@ import React, { useContext } from 'react';
 
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddres from '../hooks/useGoogleAddres';
+
 import '../styles/components/Success.css';
 
 export default function Success() {
   const {
     state: { buyer },
   } = useContext(AppContext);
+  const location = useGoogleAddres(buyer[0].address);
 
   return (
     <div className="Success">
@@ -15,7 +18,7 @@ export default function Success() {
         <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map location={location} />
         </div>
       </div>
     </div>
